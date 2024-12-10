8c3a09fa2b4037fa0b6809b372bac3417e7f1a5b
202201150-Dhrudeep <202201150@daiict.ac.in>
2024-12-11 03:12:14 +0530

    Update config.js

--- React+vite/src/config.js
+++ React+vite/src/config.js
@@ -1,6 +1,6 @@
 export const config = {
-  BACKEND_API: "",
-  // BACKEND_API: "https://appointments-1.onrender.com",
+  // BACKEND_API: "",
+  BACKEND_API: "https://appointments-1.onrender.com",
 };
 
 export default config;
